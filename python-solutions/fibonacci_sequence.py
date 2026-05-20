3319995773763d9acc7a0c7e8747db0f1d609967
Regulus Sun <little.kingsub@gmail.com>
2026-05-20 17:31:16 +0530

    Create fibonacci_sequence.py

---
+++ python-solutions/fibonacci_sequence.py
@@ -0,0 +1,24 @@
+def fibonacci_sequence(start_sequence, length):
+    if length == 0:
+        return []
+
+    if length == 1:
+        return [start_sequence[0]]  # return as an array
+
+    seq = start_sequence[:2]
+    
+    while len(seq) < length:
+        seq.append(seq[-1] + seq[-2])
+
+    return seq
+
+
+def fibonacci_sequence(start_sequence, length):
+    if length == 0:
+        return []
+
+    seq = start_sequence[:length]  # slicing the initial sequence based on length, removes the need for the separate length == 1 condition
+    while len(seq) < length:
+        seq.append(seq[-1] + seq[-2])
+
+    return seq
